704cffa2353117556ec5ad6c9f4a4ffe2681bf09
gkumar-pandey <gautamshekhar078@gmail.com>
2024-04-13 00:40:52 +0530

    feat: theme constant created

---
+++ constants/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  fontWeight: {
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+  },
+  colors: {
+    white: "#ffffff",
+    black: "#000",
+    grayBg: "#e5e5e5",
+    neutral: (opacity) => `rgbaArrayToRGBAColor(10,10,10,${opacity}`,
+  },
+  radius: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  },
+};
